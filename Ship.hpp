03299496f3f8f654a1fe3f7d320b2f1d431f6c1b
salvadorj27 <salvadormaria2@hotmail.com>
2020-10-23 22:59:20 -0400

    Nueva clase ship

---
+++ Ship.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#ifndef SHIP_HPP
+#define SHIP_HPP
+
+
+namespace Engine 
+{
+
+    class ship 
+    {
+    	public:
+			void ship_render();
+		protected:
+			int m_ship;
+	};
+
+}
+#endif // _SHIP_H_
