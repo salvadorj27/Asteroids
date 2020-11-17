43b5380aea5e650cf5a590a4336d126ef55db9de
salvadorj27 <salvadormaria2@hotmail.com>
2020-11-17 16:43:14 -0400

    Adds the Asteroid class

--- Vector2.hpp
+++ Vector2.hpp
@@ -7,28 +7,20 @@ namespace Engine
     {
         struct Vector2
         {
-            /* ==========================
-            * STATIC
-            * ==========================*/
+  
             static Vector2 Origin;
 
-            /* ==========================
-            * CONSTRUCTORS
-            * ==========================*/
+   
             Vector2();
             Vector2(float, float);
             Vector2(float);
 
-            /* ==========================
-            * FUNCTIONS
-            * ==========================*/
+  
             float Length() const;
             float SquaredLength() const;
             float Normalize();
 
-            /* ==========================
-            * CONSTRUCTORS
-            * ==========================*/
+
             float x;
             float y;
             float length;
